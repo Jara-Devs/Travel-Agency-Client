e2f1290e9df609ef90ae1b06396208e6fb100cee
ernesto.caballero <ernesto.caballero@datys.cu>
2023-11-18 23:38:23 -0500

    Terminando formulario de usuario de agencia

--- src/pages/app/UsersForm.tsx
+++ src/pages/app/UsersForm.tsx
@@ -27,8 +27,84 @@ const AgencyUserForm: FC<AgencyUserFormProps> = ({
     create,
     onOk,
     }) => {
-        
-    }
+
+    const [form] = Form.useForm<AgencyUserFormData>();
+    
+    useEffect(() => {
+        if (open) form.resetFields();
+        if (values) form.setFieldsValue({ ...values });
+    }, [open, form, values]);
+
+    return(
+     <Modal
+      open={open}
+      title={
+        <Typography>
+          <Title level={3}>Create Agency User</Title>
+        </Typography>
+      }
+      onOk={form.submit}
+      onCancel={onCancel}
+    >
+      <Form
+        layout="vertical"
+        style={{ marginTop: "20px" }}
+        form={form}
+        onFinish={(values: AgencyUserFormData) => {
+            onOk({
+                name: values.name,
+                email: values.email,
+                password: values.password,
+                role: values.role,
+                agencyId: values.agencyId
+                },
+            );
+          }
+        }
+      >
+        <Form.Item
+          name="name"
+          label="Name"
+          rules={[{ required: true, message: "Introduce the name" }]}
+        >
+          <Input placeholder="Introduce the name" />
+        </Form.Item>
+
+        <Form.Item
+          name="email"
+          label="Email"
+          rules={[{ required: true, message: "Introduce the email " }]}
+        >
+          <Input placeholder="Introduce the email" />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[{ required: true, message: "Introduce the password" }]}
+        >
+          <Input placeholder="Introduce the password" />
+        </Form.Item>
+
+        <Form.Item
+          name="role"
+          label="Role"
+          rules={[{ required: true, message: "Introduce the role" }]}
+        >
+          <Input placeholder="Introduce the role" />
+        </Form.Item>
+
+        <Form.Item
+          name="agencyId"
+          label="Agency"
+          rules={[{ required: true, message: "Introduce the agency id" }]}
+        >
+          <Input placeholder="Introduce the agency id" />
+        </Form.Item>
+      </Form>
+    </Modal>
+    )
+}
 
 export default AgencyUserForm;
 
