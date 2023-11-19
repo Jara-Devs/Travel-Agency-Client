2e8f0febb6e38a014eab5adada9bc013f7b9c8e2
ernesto.caballero <ernesto.caballero@datys.cu>
2023-11-18 23:40:06 -0500

    Removiendo referencia innecesaria

--- src/pages/app/UsersForm.tsx
+++ src/pages/app/UsersForm.tsx
@@ -2,7 +2,6 @@ import { FC, useEffect, useState } from "react";
 import {AgencyUserFormType} from "../../types/services";
 import { Form, Input, Modal, Select, Typography, message } from "antd";
 import Title from "antd/es/typography/Title";
-import { buildMessage } from "../../common/functions";
 
 export interface AgencyUserFormData {
     name: string;
