b6b46f6287b20929e00c530b9608f142f5ae4f81
ernesto.caballero <ernesto.caballero@datys.cu>
2023-11-18 23:13:19 -0500

    export interface AgencyUserFormData

--- src/pages/app/UsersForm.tsx
+++ src/pages/app/UsersForm.tsx
@@ -3,3 +3,11 @@ import {AgencyUserFormType} from "../../types/services";
 import { Form, Input, Modal, Select, Typography, message } from "antd";
 import Title from "antd/es/typography/Title";
 import { buildMessage } from "../../common/functions";
+
+export interface AgencyUserFormData {
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+    agencyId: number;
+}
