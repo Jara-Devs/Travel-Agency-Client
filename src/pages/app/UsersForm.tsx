389c1f825c947495ca48e256ac5742f3f7ade634
ernesto.caballero <ernesto.caballero@datys.cu>
2023-11-20 18:37:24 -0500

    creando interfces Data y Propos para UserForm.♠

--- src/pages/app/UsersForm.tsx
+++ src/pages/app/UsersForm.tsx
@@ -3,6 +3,24 @@ import {UserSystemFormType} from "../../types/auth";
 import { Form, Input, Modal, Select, Typography, message } from "antd";
 import Title from "antd/es/typography/Title";
 
-const UserForm: any = () => {}
+export interface UserSystemFormData {
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+    agencyId: number;
+}
+
+export interface UserSystemFormProps {
+    onOk: (form: UserSystemFormType) => void;
+    onCancel: () => void;
+    values?: UserSystemFormData;
+    create: boolean;
+    open: boolean;
+}
+
+const UserForm: any = () => {
+
+}
 
 export default UserForm;
