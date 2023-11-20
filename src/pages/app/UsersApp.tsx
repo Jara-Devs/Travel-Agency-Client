02195248fb41f95192a41e14186b4e98770b381f
ernesto.caballero <ernesto.caballero@datys.cu>
2023-11-20 18:25:40 -0500

    Importando dependencias de UsersApp.♠

--- src/pages/app/UsersApp.tsx
+++ src/pages/app/UsersApp.tsx
@@ -1,3 +1,16 @@
+import { Button, Col, Row, Tooltip, Typography, message } from "antd";
+import { userAgency } from "../../api/auth";
+import { useRef, useState } from "react";
+import Title from "antd/es/typography/Title";
+import TableEntities, { TableEntitiesRef } from "../../common/TableEntities";
+import { EditOutlined, EyeOutlined, DeleteOutlined } from "@ant-design/icons";
+import { FilterValue } from "antd/es/table/interface";
+import { Filter } from "odata-query";
+import {
+  UserSystem,
+  UserSystemFormType
+} from "../../types/auth";
+import UserForm from "./UsersForm";
 
 const UsersApp  = () => <>Users App</>;
 
