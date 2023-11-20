0ab9a8551940d075820ad6099ce42c67cdd37836
ernesto.caballero <ernesto.caballero@datys.cu>
2023-11-20 18:35:02 -0500

    Importando dependencias de UsersForm.♠

--- src/pages/app/UsersForm.tsx
+++ src/pages/app/UsersForm.tsx
@@ -1,5 +1,8 @@
+import { FC, useEffect, useState } from "react";
+import {UserSystemFormType} from "../../types/auth";
+import { Form, Input, Modal, Select, Typography, message } from "antd";
+import Title from "antd/es/typography/Title";
 
 const UserForm: any = () => {}
 
 export default UserForm;
-
