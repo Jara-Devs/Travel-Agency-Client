d6ae970a04f4844f5629521765b36a63d9d07ec2
ernesto.caballero <ernesto.caballero@datys.cu>
2023-11-23 19:09:55 -0500

    Создай интерфейс FlightOfferFormData.♠

--- src/pages/home/offers/FlightOfferForm.tsx
+++ src/pages/home/offers/FlightOfferForm.tsx
@@ -1,1 +1,12 @@
 
+export interface FlightOfferFormData {
+    name: string;
+    flightId: number;
+    availability: number;
+    description: string;
+    price: number;
+    startDate: Date;
+    endDate: Date;
+    facilities: string[];
+    imageId: number;
+}
