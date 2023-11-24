17bba31bf82cb9968c963dd58c25845ad062806e
ernesto.caballero <ernesto.caballero@datys.cu>
2023-11-23 22:29:09 -0500

    create const load.♠

--- src/pages/home/offers/FlightOfferForm.tsx
+++ src/pages/home/offers/FlightOfferForm.tsx
@@ -1,6 +1,8 @@
 import { Flight, FlightOfferFormType } from "../../../types/services";
 import { FC, useState, useEffect } from "react";
-import { Form } from "antd";
+import { Form, message } from "antd";
+import { flight } from "../../../api/services";
+import { ApiResponse } from "../../../types/api";
 
 export interface FlightOfferFormData {
     name: string;
@@ -23,12 +25,33 @@ export interface FlightOfferFormProps {
 
 const FlightOfferForm: FC<FlightOfferFormProps> = ({ onOk, onCancel, values, open }) => {
 
-    const [Flight, setFligth] = useState<Flight[]>([]);
+    const [Flight, setFlight] = useState<Flight[]>([]);
     const [form] = Form.useForm<FlightOfferFormData>();
 
     useEffect(() => {
         if (open) form.resetFields();
     }, [open, form, values]);
+
+    const load = async () => {
+        const responseFlight = await flight().get({ select: ["id", "company", "origin", "destination", "flightCategory"] });
+
+        if (responseFlight.ok) {
+            setFlight(responseFlight.value!);
+        } else {
+            let msg = "";
+
+            const aux = (response: ApiResponse<any>) => {
+                if (!responseFlight.ok) {
+                    if (msg.length === 0) msg = response.message;
+                    else msg = `${message}, ${response.message}`;
+                }
+            };
+
+            aux(responseFlight);
+
+            message.error(msg);
+        }
+    };
 }
 
 export default FlightOfferForm;
