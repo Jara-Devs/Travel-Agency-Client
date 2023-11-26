cf09c719720d9d9990553e4c9b86109e23231517
alexsierra45 <alexsierraalcala@gmail.com>
2023-11-26 14:00:53 -0500

    adding dependencies

--- src/pages/home/offers/ExcursionOffer.tsx
+++ src/pages/home/offers/ExcursionOffer.tsx
@@ -1,3 +1,21 @@
+import { Col, Row, Tooltip, Typography, message } from "antd";
+import ShowEntities from "../../../common/ShowEntities";
+import { useEffect, useState } from "react";
+import { ExcursionOfferType, ReactionState } from "../../../types/offers";
+import SlideCard from "../../../common/SlideCard";
+import ShowExcursion, { ShowMiniExcursion } from "../../show/services/ShowExcursion";
+import { EyeOutlined, LikeFilled, DislikeFilled } from "@ant-design/icons";
+import { excursionOffer } from "../../../api/offers";
+import ShowExcursionOffer from "../../show/offers/ShowExcursionOffer";
+import { Excursion, OverNighExcursion } from "../../../types/services";
+import { useSearchParams } from "react-router-dom";
+import { Filter } from "odata-query";
+import { excursion, overNighExcursion } from "../../../api/services";
+import FilterSearch, { FilterItem } from "../../../common/FilterSearch";
+import { UserContext } from "../../../context/UserProvider";
+import { useContext } from "react";
+import { reactionLogic, selectedReaction } from "../../../common/functions";
+
 const ExcursionOffer = () => <>Excursion Offer</>;
 
 export default ExcursionOffer;
