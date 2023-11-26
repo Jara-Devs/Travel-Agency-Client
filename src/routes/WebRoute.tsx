692f06ab22380d72abbeba2a2588603c77285cc8
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-11-26 13:42:40 -0500

    new loyout

--- src/routes/WebRoute.tsx
+++ src/routes/WebRoute.tsx
@@ -1,9 +1,4 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { Layout } from "antd";
-import { Content, Header } from "antd/es/layout/layout";
-import MyHeader from "../layout/Header";
-import { Sidebar } from "../layout/Sidebar";
-import Sider from "antd/es/layout/Sider";
 import OfferRoute from "./OfferRoute";
 import ServiceRoute from "./ServiceRoute";
 import Packages from "../pages/home/packages/Packages";
@@ -15,48 +10,34 @@ import { Roles } from "../types/auth";
 
 export const WebRouter = () => {
   return (
-    <Layout className="layout">
-      <Sider width={275}>
-        <Sidebar />
-      </Sider>
-
-      <Layout>
-        <Header className="layout-header">
-          <MyHeader />
-        </Header>
-
-        <Content className="layout-content">
-          <Routes>
-            <Route path="/offer/*" element={<OfferRoute />}></Route>
-            <Route path="/service/*" element={<ServiceRoute />}></Route>
-            <Route path="/package" element={<Packages />}></Route>
-            <Route
-              path="/app/*"
-              element={
-                <PrivateRoutes
-                  requiredRoles={[Roles.AdminApp, Roles.EmployeeApp]}
-                  component={AppRoute}
-                />
-              }
-            ></Route>
-            <Route
-              path="/agency/*"
-              element={
-                <PrivateRoutes
-                  requiredRoles={[
-                    Roles.AdminAgency,
-                    Roles.ManagerAgency,
-                    Roles.EmployeeAgency,
-                  ]}
-                  component={AgencyRoute}
-                />
-              }
-            ></Route>
-            <Route path="/" element={<HomeView />}></Route>
-            <Route path="*" element={<Navigate to="/" />}></Route>
-          </Routes>
-        </Content>
-      </Layout>
-    </Layout>
+    <Routes>
+      <Route path="/offer/*" element={<OfferRoute />}></Route>
+      <Route path="/service/*" element={<ServiceRoute />}></Route>
+      <Route path="/package" element={<Packages />}></Route>
+      <Route
+        path="/app/*"
+        element={
+          <PrivateRoutes
+            requiredRoles={[Roles.AdminApp, Roles.EmployeeApp]}
+            component={AppRoute}
+          />
+        }
+      ></Route>
+      <Route
+        path="/agency/*"
+        element={
+          <PrivateRoutes
+            requiredRoles={[
+              Roles.AdminAgency,
+              Roles.ManagerAgency,
+              Roles.EmployeeAgency,
+            ]}
+            component={AgencyRoute}
+          />
+        }
+      ></Route>
+      <Route path="/" element={<HomeView />}></Route>
+      <Route path="*" element={<Navigate to="/" />}></Route>
+    </Routes>
   );
 };
