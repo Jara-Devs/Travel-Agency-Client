3cf479102226319e80b089d150fdee6504c82967
alexsierra45 <alexsierraalcala@gmail.com>
2023-11-26 02:44:40 -0500

    excursion offer

---
+++ src/pages/agency/offers/excursion/ExcursionOffer.tsx
@@ -0,0 +1,275 @@
+import { Button, Col, Row, Tag, Tooltip, Typography, message } from "antd";
+import { useRef, useState } from "react";
+import Title from "antd/es/typography/Title";
+import { EditOutlined, EyeOutlined, DeleteOutlined } from "@ant-design/icons";
+import { FilterValue } from "antd/es/table/interface";
+import ExcursionOfferForm, { excursionLabel } from "./ExcursionOfferForm";
+import {
+  ExcursionOfferFormType,
+  ExcursionOfferType,
+} from "../../../../types/services";
+import ShowExcursionOffer from "../../../show/offers/ShowExcursionOffer";
+import TableEntities, {
+  TableEntitiesRef,
+} from "../../../../common/TableEntities";
+import dayjs from "dayjs";
+import { excursionOffer } from "../../../../api/services";
+import { getExcursionFacility } from "../../../../common/functions";
+import { Filter } from "odata-query";
+
+const ExcursionOffer = () => {
+  const { get, create, edit, remove } = excursionOffer();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [createModal, setCreateModal] = useState<boolean>(false);
+  const [editModal, setEditModal] = useState<boolean>(false);
+
+  const [selected, setSelected] = useState<ExcursionOfferType>();
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const tableRef = useRef<TableEntitiesRef>({
+    reload: () => {},
+    reset: () => {},
+  });
+
+  const load = async (
+    _: Record<string, FilterValue | null>,
+    search: string,
+    setDataValue: (data: ExcursionOfferType[]) => void
+  ) => {
+    setLoading(true);
+
+    const searchFilter: Filter = { excursion: { name: { contains: search } } };
+
+    const response = await get({
+        expand: {
+          image: {
+            select: ["id", "name", "url"],
+          },
+          excursion: {
+            select: ["name"],
+            expand: {
+                image: {
+                    select: ["id", "name", "url"],
+                },
+                places: {
+                    select: ["id", "name"],
+                },
+                activities: {
+                    select: ["id", "name"],
+            },
+          },
+        }},
+  
+        filter: searchFilter,
+    });
+
+    if (response.ok) {
+      const data = response.value || [];
+      setDataValue(data);
+    } else {
+      message.error(response.message);
+    }
+    setLoading(false);
+  };
+
+  const createexcursionOffer = async (form: ExcursionOfferFormType) => {
+    setLoading(true);
+    const response = await create(form);
+
+    if (response.ok) {
+      message.success("Excursion Offer created");
+      tableRef.current.reload();
+    } else message.error(response.message);
+    setLoading(false);
+  };
+
+  const editexcursionOffer = async (form: ExcursionOfferFormType, id: number) => {
+    setLoading(true);
+    const response = await edit(form, id);
+
+    if (response.ok) {
+      message.success("Excursion Offer edited");
+      tableRef.current.reload();
+    } else message.error(response.message);
+    setLoading(false);
+  };
+
+  const deleteexcursionOffer = async (id: number) => {
+    setLoading(true);
+    const response = await remove(id);
+
+    if (response.ok) {
+      message.success("Excursion Offer deleted");
+      tableRef.current.reload();
+    } else message.error(response.message);
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <div className="m-5">
+        <Row justify="space-between" className="app-header">
+          <Col>
+            <Typography>
+              <Title>Excursions</Title>
+            </Typography>
+          </Col>
+          <Col>
+            <Button
+              type="primary"
+              disabled={loading}
+              onClick={() => setCreateModal(true)}
+            >
+              Create
+            </Button>
+          </Col>
+        </Row>
+        <Row className="content-center m-10">
+          <Col span={24}>
+            <TableEntities
+              ref={tableRef}
+              title="Excursions"
+              loading={loading}
+              columns={[
+                {
+                  title: "Name",
+                  key: "name",
+                  render: (v: ExcursionOfferType) => <>{v.name}</>,
+                },
+                {
+                  title: "Excursion",
+                  key: "excursion",
+                  render: (v: ExcursionOfferType) => <>{excursionLabel(v.excursion)} </>,
+                },
+                {
+                  title: "Availability",
+                  key: "availability",
+                  render: (v: ExcursionOfferType) => <>{v.availability}</>,
+                },
+                {
+                  title: "Description",
+                  key: "description",
+                  render: (v: ExcursionOfferType) => <>{v.description}</>,
+                },
+                {
+                  title: "Price",
+                  key: "price",
+                  render: (v: ExcursionOfferType) => <>{v.price}</>,
+                },
+                {
+                  title: "Initial Date",
+                  key: "startDate",
+                  render: (v: ExcursionOfferType) => (
+                    <>{dayjs(v.startDate).format("DD/MM/YYYY")}</>
+                  ),
+                },
+                {
+                  title: "Final Date",
+                  key: "endDate",
+                  render: (v: ExcursionOfferType) => (
+                    <>{dayjs(v.endDate).format("DD/MM/YYYY")}</>
+                  ),
+                },
+                {
+                  title: "Facilities",
+                  key: "facilities",
+                  render: (v: ExcursionOfferType) => (
+                    <>
+                      <Row>
+                        {v.facilities.map((f, idx) => (
+                          <Tag key={idx} color="blue">
+                            {getExcursionFacility(f)}
+                          </Tag>
+                        ))}
+                      </Row>
+                    </>
+                  ),
+                },
+
+                {
+                  title: "Actions",
+                  key: "Actions",
+                  render: (v: ExcursionOfferType) => (
+                    <Row gutter={10}>
+                      <Col>
+                        <Tooltip title="Show">
+                          <EyeOutlined
+                            onClick={() => {
+                              setSelected(v);
+                              setShowModal(true);
+                            }}
+                          />
+                        </Tooltip>
+                      </Col>
+                      <Col>
+                        <Tooltip title="Edit">
+                          <EditOutlined
+                            onClick={() => {
+                              setSelected(v);
+                              setEditModal(true);
+                            }}
+                          />
+                        </Tooltip>
+                      </Col>
+                      <Col>
+                        <Tooltip title="Delete">
+                          <DeleteOutlined
+                            onClick={() => {
+                              deleteexcursionOffer(v.id);
+                            }}
+                          />
+                        </Tooltip>
+                      </Col>
+                    </Row>
+                  ),
+                },
+              ]}
+              load={load}
+            />
+          </Col>
+        </Row>
+      </div>
+      <ExcursionOfferForm
+        onOk={(form: ExcursionOfferFormType) => {
+          setCreateModal(false);
+          createexcursionOffer(form);
+        }}
+        onCancel={() => setCreateModal(false)}
+        open={createModal}
+      />
+      {selected && (
+        <ExcursionOfferForm
+          onOk={(form: ExcursionOfferFormType) => {
+            setEditModal(false);
+            editexcursionOffer(form, selected.id);
+          }}
+          onCancel={() => setEditModal(false)}
+          open={editModal}
+          values={{
+            name: selected.name,
+            excursionId: selected.excursionId,
+            availability: selected.availability,
+            description: selected.description,
+            price: selected.price,
+            startDate: dayjs(selected.startDate),
+            endDate: dayjs(selected.endDate),
+            facilities: selected.facilities,
+            image: selected.image,
+          }}
+        />
+      )}
+      {selected && (
+        <ShowExcursionOffer
+          open={showModal}
+          onOk={() => {
+            setShowModal(false);
+          }}
+          excursionOffer={selected}
+        />
+      )}
+    </>
+  );
+};
+
+export default ExcursionOffer;
